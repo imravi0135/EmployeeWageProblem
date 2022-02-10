49eddde776caf367d7c6a04acde30ba0aa5fefce
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-02-10 17:06:10 +0530

    commit message

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 //EmployeeWageProblem.UC2_EmployeeDailyWage.GetAttendence();
-EmployeeWageProblem.EmpWageUsingSwitch.EmpWage();
+//EmployeeWageProblem.UC4_EmpWageUsingSwitch.EmpWage();
+EmployeeWageProblem.EmployeeWageProblem.CalculateWageForMonth.CalculateWagesFor20DaysInMonth();
 
--- EmployeeWageProblem/UC5_CalculateWageForMonth.cs
+++ EmployeeWageProblem/UC5_CalculateWageForMonth.cs
@@ -37,6 +37,7 @@ namespace EmployeeWageProblem
             Console.ReadKey();
         }
 
+
         public static void PartTimeEmpWage()
         {
             //Constants
@@ -102,6 +103,53 @@ namespace EmployeeWageProblem
                 dailyWage = dailyHours * RATE_PER_HOUR;
                 Console.WriteLine("Daily Wage: " + dailyWage);
             }
+
         }
     }
+        namespace EmployeeWageProblem
+
+    {
+        internal class CalculateWageForMonth
+        {//Constants
+            public const int IS_PART_TIME = 1;
+            public const int IS_FULL_TIME = 2;
+            public const int EMP_RATE_PER_HOUR = 20;
+            public const int NUM_OF_WORKING_DAYS = 20;
+            public static void CalculateWagesFor20DaysInMonth()
+            {
+
+                //variables
+                int empHrs = 0;
+                int empWage = 0;
+                int totalEmpWage = 0;
+                for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+                {
+
+                    Random random = new Random();
+                    int empCheck = random.Next(0, 3);
+                    switch (empCheck)
+                    {
+                        case IS_PART_TIME:
+                            empHrs = 4;
+                            break;
+                        case IS_FULL_TIME:
+                            empHrs = 8;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    totalEmpWage += empWage;   //int a=6; a+=10;a=a+10
+                    Console.WriteLine("Emp Wage: " + empWage);
+                }
+                Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+
+            }
+        }
+    
+
+    }
+
+
 }
