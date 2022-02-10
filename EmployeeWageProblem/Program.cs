ef44ada26f098a52ce5b02961bb9a34362c13558
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-02-10 15:42:53 +0530

    commit message

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 //EmployeeWageProblem.UC2_EmployeeDailyWage.GetAttendence();
-EmployeeWageProblem.UC2_EmployeeDailyWage.EmpWage();
+EmployeeWageProblem.UC3_PartTimeEmpWage.EmpWage();
+
